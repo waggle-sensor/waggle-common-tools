80f9ba50b873806a2316efc437026c385c39fc4a
Joe Swantek <joseph.swantek@northwestern.edu>
2021-01-21 17:45:45 -0600

    correct "version" in filename, add architecture, and set root ownership
    Underscores ('_') are used as delimiters in a Debian package filename
    and therefore can't be used in the "version" field. Files in the
    Debian package need to be owned by the root user/group so that they
    will accessible on the installed system.

--- build.sh
+++ build.sh
@@ -8,15 +8,18 @@ fi
 GIT_SHA=$(git rev-parse --short HEAD)
 
 build_dir=$(mktemp -d)
+NAME=sage-common-tools
+ARCH=all
+VERSION=${RELEASE_VERSION}-${GIT_SHA}
 
 # add package description
 mkdir -p $build_dir/DEBIAN
 cat <<EOF > $build_dir/DEBIAN/control
-Package: sage-common-tools
-Version: ${RELEASE_VERSION}-${GIT_SHA}
+Package: ${NAME}
+Version: ${VERSION}
 Maintainer: sagecontinuum.org
 Description: Common tools for Sage OS administration and operations.
-Architecture: all
+Architecture: ${ARCH}
 Priority: optional
 EOF
 
@@ -25,4 +28,4 @@ mkdir -p $build_dir/usr/bin
 cp tools/* $build_dir/usr/bin
 
 # build deb
-dpkg-deb --build $build_dir "sage-common-tools_${RELEASE_VERSION}_${GIT_SHA}.deb"
+dpkg-deb --root-owner-group --build $build_dir "${NAME}_${VERSION}_${ARCH}.deb"
